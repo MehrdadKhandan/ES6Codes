434991a8b692f6df5f6d3c5cec56d13f611be0e3
MehrdadKhandan <mehrdadkhandan124@gmail.com>
2018-05-20 12:27:23 +0430

    Switch and case code

---
+++ SwitchCase.js
@@ -0,0 +1,15 @@
+function nn(num){
+    var x;
+ switch(num%2){
+case 0:
+  x = "the number is even";
+  break;
+  case 1:
+  x = "the number is odd";
+  break;
+  default:
+  x = " dont enter anythings";
+  break;
+ }
+return x;
+}
